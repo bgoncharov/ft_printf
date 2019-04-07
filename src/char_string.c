b7be0c37d410c4670af3e78d80a1c93749a528db
Boris G <bora4bora@gmail.com>
2019-04-07 13:01:57 -0700

    string format

--- src/char_string.c
+++ src/char_string.c
@@ -6,7 +6,7 @@
 /*   By: bogoncha <bogoncha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/27 13:07:57 by bogoncha          #+#    #+#             */
-/*   Updated: 2019/04/01 16:27:52 by bogoncha         ###   ########.fr       */
+/*   Updated: 2019/04/07 12:59:00 by bogoncha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 /*
 ** correttly cut the string
-*/
 static void add_prec(int prec, char **str)
 {
 	int len;
@@ -28,6 +27,7 @@ static void add_prec(int prec, char **str)
 		*str = new;
 	}
 }
+*/
 
 static void	add_width(int width, char **str, int left)
 {
@@ -47,7 +47,24 @@ static void	add_width(int width, char **str, int left)
 	}
 }
 
-char *flg_char(t_format *frmt_struct, va_list args)
+static char *frmt_str(t_format *frmt, int len)
+{
+	if (frmt->precision != -1)
+	{
+		if (frmt->precision > len)
+			frmt->precision = len;
+		if (frmt->precision > frmt->width)
+			frmt->width = frmt->precision;
+	}
+	else
+	{
+		frmt->width = ft_max(frmt->width, len);
+		frmt->precision = len;
+	}
+	return (ft_strinitial(frmt->width, ' '));
+}
+
+char *flg_char(t_format *frmt, va_list args)
 {
 	char c;
 	char *new;
@@ -57,30 +74,38 @@ char *flg_char(t_format *frmt_struct, va_list args)
 	if (!c)
 		new = ft_strdup("^@");
 	new = ft_strndup(&c, 1);
-	add_width(frmt_struct->width, &new, (frmt_struct->flags & MINUS));
-	return (ft_strdup(&c));
+	add_width(frmt->width, &new, (frmt->flags & MINUS));
+	return (new);
 }
 
-char *flg_str(t_format *frmt_struct, va_list args)
+char *flg_str(t_format *frmt, va_list args)
 {
 	char *str;
+	char *new;
+	int len;
 
 	//print_params(*fmt_struct);
 	str = va_arg(args, char *);
 	if (!str)
-		str = ft_strdup("(null)");
+	{
+		str = "(null)";
+		len = 6;
+	}
 	else
-		str = ft_strdup(str);
-	add_prec(frmt_struct->precision, &str);
-	add_width(frmt_struct->width, &str, (frmt_struct->flags & MINUS));
-	return (str);
+		len = ft_strlen(str);
+	new = frmt_str(frmt, len);
+	if (frmt->flags & MINUS)
+		ft_strncpy(new, str, frmt->precision);
+	else
+		ft_strncpy(new + frmt->width - frmt->precision, str, frmt->precision);	
+	return (new);
 }
 
-char *flg_percent(t_format *frmt_struct)
+char *flg_percent(t_format *frmt)
 {
 	char *str;
 	//print_params(*fmt_struct);
 	str = ft_strdup("%");
-	add_width(frmt_struct->width, &str, (frmt_struct->flags & MINUS));
+	add_width(frmt->width, &str, (frmt->flags & MINUS));
 	return (str);
 }
