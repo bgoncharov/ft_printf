116bbbea54a0bf10a14071ff1659f53d597419ed
Boris Goncharov <bora4bora@gmail.com>
2019-04-13 23:13:46 -0700

    hex

--- src/flg_hex.c
+++ src/flg_hex.c
@@ -1,29 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   flg_oct.c                                          :+:      :+:    :+:   */
+/*   flg_hex.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bogoncha <bogoncha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/11 17:17:56 by bogoncha          #+#    #+#             */
-/*   Updated: 2019/04/11 17:39:54 by bogoncha         ###   ########.fr       */
+/*   Created: 2019/04/13 22:51:34 by bogoncha          #+#    #+#             */
+/*   Updated: 2019/04/13 23:13:18 by bogoncha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-static unsigned long long take_num(char conv, char length, va_list args)
+static unsigned long long take_num(char length, va_list args)
 {
 	unsigned long long num;
 
 	num = 0;
-	if (conv == 'O')
-	{
-		if (length == 'l' || length == 'L')
-			length = 'L';
-		else
-			length = 'l';
-	}
 	if (length == 0)
 		num = va_arg(args, unsigned int);
 	else if (length == 'l')
@@ -41,32 +34,39 @@ static unsigned long long take_num(char conv, char length, va_list args)
 	return (num);
 }
 
-static void	append_flags(char *str, t_format *frmt, int len)
+static void	append_flags(char *str, t_format *frmt,
+                            unsigned long long num, int len)
 {
 	if (!(frmt->flags & MINUS))
 		str += (frmt->width - frmt->precision);
-    if (frmt->flags & SHARP && len == 0)
-        ++len;
-	if (len)
-	{
-		while (str && *str == ' ')
-			*str++ = '0';
-	}
+    if (len)
+    {
+        if (frmt->flags & SHARP && len == 0)
+        {
+            ft_strncpy(str, "0x", 2);
+            str += 2;
+        }
+        while (str && *str == ' ')
+            *str++ = '0';
+    }
 }
 
-static void	ft_numcpy(unsigned long long num, char *str)
+static void	ft_numcpy_hex(unsigned long long num, char *str)
 {
-	if (num >= 8)
-		ft_numcpy_oct(num / 8, str - 1);
-	*str = (num % 8 + '0');
+	if (num >= 16)
+		ft_numcpy_hex(num / 16, str - 1);
+    if ((num % 16) >= 10)
+        *str = ((num % 16) + 'a' - 10);
+    else
+	    *str = (num % 16 + '0');
 }
 
 static char	*num_format(t_format *frmt, unsigned long long num, int len)
 {
+    if (frmt->flags & SHARP && num != 0)
+		len += 2;
 	if (frmt->precision != -1)
 	{
-        if (frmt->flags & SHARP && frmt->precision <= len)
-            ++len;
 		if (frmt->precision < len)
 			frmt->precision = len;
 		if (frmt->precision > frmt->width)
@@ -74,8 +74,6 @@ static char	*num_format(t_format *frmt, unsigned long long num, int len)
 	}
 	else
 	{
-        if (frmt->flags & SHARP && num != 0)
-            ++len;
 		frmt->width = ft_max(frmt->width, len);
 		frmt->precision = len;
 		if (frmt->flags & ZERO)
@@ -83,25 +81,28 @@ static char	*num_format(t_format *frmt, unsigned long long num, int len)
 	}
 	return (ft_strinitial(frmt->width, ' '));
 }
-char		*flg_oct(t_format *frmt, va_list args)
+
+char		*flg_hex(t_format *frmt, va_list args)
 {
 	char		*new;
 	long long	num;
     int			len;
 
-	num = take_num(frmt->conv, frmt->lenght, args);
+	num = take_num(frmt->lenght, args);
     if (frmt->precision == 0 && num == 0)
         len = 0;
     else
-        len = ft_numberlen_base(num, 8);    
+        len = ft_numberlen_base(num, 16);    
 	new = num_format(frmt, num, len);
     if (len)
     {
         if (frmt->flags & MINUS)
-            ft_numcpy_oct(num, new + (frmt->precision - 1));
+            ft_numcpy_hex(num, new + (frmt->precision - 1));
         else
-            ft_numcpy_oct(num, new + (frmt->width - 1));
+            ft_numcpy_hex(num, new + (frmt->width - 1));
     }
-	append_flags(new, frmt, len);
+	append_flags(new, frmt, num, len);
+    if (ft_isupper(frmt->conv))
+        ft_strupcase(new);
 	return (new);
 }
