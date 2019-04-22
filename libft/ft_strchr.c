9b7b831a9321074868ccd153215586c48f67a4bc
Boris Goncharov <bora4bora@gmail.com>
2019-04-22 10:38:07 -0700

    fixes

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: bogoncha <bogoncha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 18:26:07 by bogoncha          #+#    #+#             */
-/*   Updated: 2019/02/20 18:41:08 by bogoncha         ###   ########.fr       */
+/*   Updated: 2019/04/22 10:37:07 by bogoncha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ char	*ft_strchr(const char *string, int symbol)
 {
 	while (*string)
 	{
-		if (*string == symbol)
-			return ((char*)string);
+		if (*string == (char)symbol)
+			return ((char *)(string));
 		++string;
 	}
-	if (symbol == '\0')
-		return ((char*)string);
-	return (0);
+	return ((char)symbol == '\0' ? (char *)string : 0);
 }
